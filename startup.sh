01e0c39911a9882cf8d4e7fa1ee51d81f3bfbd80
Lawrence Dudley <lawrence@parall.ax>
2019-04-05 11:57:16 +0100

    rm password, add ip auth

--- startup.sh
+++ startup.sh
@@ -1,12 +1,3 @@
 #!/bin/sh
 
-if [ -z "$ADMIN_PASSWORD" ]; then
-        
-    echo "No ADMIN_PASSWORD provided for the web interface - exiting"
-    exit
-fi
-
-echo "Creating auth file in /home/mailhog/auth-file"
-htpasswd -Bbc /home/mailhog/auth-file admin $ADMIN_PASSWORD
-
-/usr/local/bin/MailHog -auth-file=/home/mailhog/auth-file -outgoing-smtp=/home/mailhog/outgoing.json
+/usr/local/bin/MailHog -outgoing-smtp=/home/mailhog/outgoing.json
